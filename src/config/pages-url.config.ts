24f8c35f13384beb908b12850e671bc6f7598ac9
andreydos <andrii3110@gmail.com>
2024-07-18 17:02:29 -0300

    types added and layout update

---
+++ src/config/pages-url.config.ts
@@ -0,0 +1,9 @@
+class DASHBOARD {
+	private root = '/i'
+	
+	HOME = this.root
+	TASKS = `${this.HOME}/tasks`
+	HABITS = `${this.HOME}/habits`
+}
+
+export const DASHBOARD_PROVIDERS = new DASHBOARD()
