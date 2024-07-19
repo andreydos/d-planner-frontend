5446787a282aae4c17441080cb3d42f32a43cedb
andreydos <andrii3110@gmail.com>
2024-07-18 21:47:38 -0300

    layout for dashboard added

--- src/app/i/page.tsx
+++ src/app/i/page.tsx
@@ -1,11 +1,21 @@
-import type {Metadata} from 'next'
-import {NO_INDEX_PAGE} from '@/constants/seo.constants'
+import type { Metadata } from 'next'
+
+import { Heading } from '@/components/ui/Heading'
+
+import { NO_INDEX_PAGE } from '@/constants/seo.constants'
+
+import { Statistics } from './Statistics'
 
 export const metadata: Metadata = {
 	title: 'Dashboard',
-	...NO_INDEX_PAGE,
+	...NO_INDEX_PAGE
 }
 
-export default function DashboardPage () {
-	return <div>Dashboard</div>
+export default function DashboardPage() {
+	return (
+		<div>
+			<Heading title='Statistics' />
+			<Statistics />
+		</div>
+	)
 }
